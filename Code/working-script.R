904a94ad340bd5f27263a25a5394227bbbfe2b07
Bree Norlander <norlab@users.noreply.github.com>
2024-03-01 15:17:27 -0800

    updates

--- Code/working-script.R
+++
@@ -1,5 +0,0 @@
-sites |> 
-  separate_longer_delim(`Years of AmeriFlux FLUXNET Data`, delim=", ") |>
-  group_by(`Site ID`) %>%
-  mutate(Diff_FLUXNET = as.numeric(`Years of AmeriFlux FLUXNET Data`) - lag(as.numeric(`Years of AmeriFlux FLUXNET Data`))) |> 
-  View()
